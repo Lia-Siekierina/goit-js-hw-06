e94bfcc2571e84ee5a4dae7976d2e0d5156cb05d
Lia <natalimail911@gmail.com>
2024-09-26 17:34:17 +0300

    task3

--- js/task-3.js
+++ js/task-3.js
@@ -8,13 +8,14 @@ class StringBuilder {
         return this.#value;
     }
     padEnd(str) {
-        
+        this.#value += str;
     }
     padStart(str) {
-
+        this.#value = str + this.#value;
     }
     padBoth(str) {
-
+        this.padStart(str);
+        this.padEnd(str);
     }
 }
 
